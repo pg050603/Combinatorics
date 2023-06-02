53c9bd5354489d6b6399ba2391a191fd07e89e57
Perry Giles <pgil091@UoA.auckland.ac.nz>
2023-06-02 15:52:34 +1200

    Finishedgit add .

--- task2_lab6.py
+++ task2_lab6.py
@@ -1,18 +1,66 @@
 from module_lab6 import *
-
 network = Network()
 network.read_network('network_waka_voyage.txt')
-
-source_name = 'Taiwan'
-destination_name = 'Hokianga'
-
-distance, path = spath_algorithm(network, source_name, destination_name)
-
-print(f'The distance between {source_name} and {destination_name} is: {distance}')
-print(f'The path to get to {destination_name} from {source_name} is: {path}')
-
-# Island = Network.get_node(network, 'Taiwan')
-#
-# print(Island)
-
 # TODO - your code here
+
+# source_name = 'Taiwan'
+# destination_name = 'Hokianga'
+#
+# a,b = spath_algorithm(network, source_name, destination_name)
+#
+# print(a)
+# print(b)
+
+mapping = {}
+for node in network.nodes:
+    source_name = network.get_node(node.name).name
+    for node in network.nodes:
+        destination_name = network.get_node(node.name).name
+        distance, path = spath_algorithm(network, source_name, destination_name)
+        #Figure out how to have condition to check if key exists
+        if key exists:
+            mapping[distance].append(path)
+        else:
+            mapping[distance] = [path]
+
+del mapping[None]
+
+max_value = mapping[max(mapping)]
+
+print(max_value)
+
+
+# filtered_distances = filter(lambda item: item is not None, distances)
+# new_lst = list(filtered_distances)
+#
+# longest_path = max(new_lst)
+#
+# print(longest_path)
+
+
+
+
+# # Helper function: Returns the distance value of a node with specified name
+# def get_distance(distance):
+#     return distance
+#
+# # Return the node in unvisited with the minimum distance
+# max_dist = max(distances)
+#
+# print(max_dist)
+#
+#
+#
+#
+# print(paths)
+# print(distances)
+#
+#
+# #         print(f'The distance between {source_name} and {destination_name} is: {distance}')
+# #         print(f'The path to get to {destination_name} from {source_name} is: {path}')
+# #
+# # Bismarck Sea and Hawaii is: 82.0
+# # distance between Palau and Hawaii is: 92.0
+# 103.0
+
+
